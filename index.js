910017214a5e06e0af87638e1ea4c9500b012887
siddubala26 <64766682+siddubala26@users.noreply.github.com>
2020-05-31 17:25:49 +0530

    Add initial changs

---
+++ index.js
@@ -0,0 +1,62 @@
+var nodb = document.querySelectorAll(".drum").length;
+for (var i = 0; i < nodb; i++) {
+  document.querySelectorAll(".drum")[i].addEventListener("click", function() {
+
+    var binn = this.innerHTML
+
+
+    cvbn(binn);
+  });
+
+}
+document.addEventListener("keypress", function(event) {
+cvbn(event.key);
+});
+
+
+function cvbn(dfg) {
+  switch (dfg) {
+    case "w":
+      var audio = new Audio('sounds/tom-1.mp3');
+      audio.play();
+
+      break;
+    case "a":
+      var audio = new Audio('sounds/tom-2.mp3');
+      audio.play();
+      break;
+
+
+    case "s":
+      var audio = new Audio('sounds/tom-3.mp3');
+      audio.play();
+      break;
+
+
+    case "d":
+      var audio = new Audio('sounds/tom-4.mp3');
+      audio.play();
+      break;
+
+
+    case "j":
+      var audio = new Audio('sounds/snare.mp3');
+      audio.play();
+      break;
+
+
+    case "k":
+      var audio = new Audio('sounds/crash.mp3');
+      audio.play();
+      break;
+
+
+    case "l":
+      var audio = new Audio('sounds/kick-bass.mp3');
+      audio.play();
+      break;
+
+    default:
+
+  }
+}
